0be53d8e96c6ea81d84338f05f0ff2960c705be4
GxBonus <bthao018@gmail.com>
2022-06-21 11:32:13 -0500

    created deleteAllByQuizTemplateId to have deleteQuiz, change createQuiz to void

--- quiz/src/main/java/net/yorksolutions/quiz/QuizRepository.java
+++ quiz/src/main/java/net/yorksolutions/quiz/QuizRepository.java
@@ -3,9 +3,12 @@ package net.yorksolutions.quiz;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.Optional;
 
 @Repository
 public interface QuizRepository extends CrudRepository<Quiz, Long> {
-    Optional<Quiz> findByQuizTemplateId(Long quizTemplateId);
+    //Optional<Quiz> findByQuizTemplateId(Long quizTemplateId);
+    @Transactional
+    Iterable<Quiz> deleteAllByQuizTemplateId(Long quizTemplateId);
 }
--- quiz/src/main/java/net/yorksolutions/quiz/QuizServices.java
+++ quiz/src/main/java/net/yorksolutions/quiz/QuizServices.java
@@ -16,18 +16,17 @@ public class QuizServices {
         this.repository = repository;
     }
 
-    public Quiz createQuiz(Long quizTempId, int questionNumber, String questionText, String questionType) {
+    public void createQuiz(Long quizTempId, int questionNumber, String questionText, String questionType) {
         Quiz newQuizCreated = new Quiz(quizTempId, questionNumber, questionText, questionType);
         repository.save(newQuizCreated);
-        return newQuizCreated;
     }
-    public Quiz createQuizA(Quiz quiz){
-        return repository.save(quiz);
+    public void createQuizA(Quiz quiz) {
+        repository.save(quiz);
     }
 
-    public void editQuiz(Long quizId, String questionText, String questionType){
-        Optional<Quiz> findQuiz = repository.findById(quizId);
-        if (findQuiz.isEmpty()){
+    public void editQuiz(Long questionId, String questionText, String questionType) {
+        Optional<Quiz> findQuiz = repository.findById(questionId);
+        if (findQuiz.isEmpty()) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         }else{
             Quiz currentQuiz = findQuiz.get();
@@ -37,23 +36,17 @@ public class QuizServices {
         }
     }
 
-    public void deleteQuestion(Long quizId){
-        repository.deleteById(quizId);
+    public void deleteQuestion(Long questionId) {
+        repository.deleteById(questionId);
     }
 
-    public void deleteQuiz(Long quizId){
-        Optional<Quiz> existingQuiz = repository.findByQuizTemplateId(quizId);
+    public void deleteQuiz(Long quizTempId) {
 
-        if (existingQuiz.isEmpty()){
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-        }else{
-            Long quizTemplateId = existingQuiz.get().getQuizTemplateId();
-            //Quiz quizTemplateId = existingQuiz.get();
-            repository.deleteById(quizTemplateId);
-        }
+        repository.deleteAllByQuizTemplateId(quizTempId);
+
     }
 
-    public Iterable<Quiz> getAllQuizzes(){
+    public Iterable<Quiz> getAllQuizzes() {
         return repository.findAll();
     }
 
