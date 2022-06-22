45b8a4a03aa0846cd69f5e6b3f7a23170f48e1ce
GxBonus <bthao018@gmail.com>
2022-06-22 16:33:05 -0500

    test case for createQuiz, deleteQuestion, deleteQuiz and getAllQuizzes

--- quiz/src/test/java/net/yorksolutions/quiz/QuizControllerTest.java
+++ quiz/src/test/java/net/yorksolutions/quiz/QuizControllerTest.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.server.ResponseStatusException;
@@ -35,19 +37,19 @@ class QuizControllerTest {
         controller.setService(service);
     }
 
-//    @Test
-//    void itShouldCallCreateQuizFromServiceWithQuizObjReturnGetAllQuizzes(){
-//        Quiz someQuiz = new Quiz();
-//        ArrayList<Quiz> expectedQuiz = new ArrayList<>();
-//        TestRestTemplate rest = new TestRestTemplate();
-//        final String url = "http://localhost:" + port + "/createQuiz";
-//        lenient().doNothing().when(service).createQuiz(someQuiz);
-//        when(service.getAllQuizzes()).thenReturn(expectedQuiz);
-//        ResponseEntity<Void> response = rest.postForEntity(url,)
-//        Iterable<Quiz> result = service.getAllQuizzes();
-//        assertEquals(expectedQuiz, result);
-//        assertEquals(HttpStatus.OK, response.getStatusCode());
-//    }
+    @Test
+    void itShouldCallCreateQuizFromServiceWithQuizObjReturnGetAllQuizzes(){
+        Quiz someQuiz = new Quiz();
+        ArrayList<Quiz> expectedQuiz = new ArrayList<>();
+        TestRestTemplate rest = new TestRestTemplate();
+        final String url = "http://localhost:" + port + "/createQuiz";
+        lenient().doNothing().when(service).createQuiz(someQuiz);
+        when(service.getAllQuizzes()).thenReturn(expectedQuiz);
+        ResponseEntity<Void> response = rest.postForEntity(url,someQuiz, Void.class);
+        Iterable<Quiz> result = service.getAllQuizzes();
+        assertEquals(expectedQuiz, result);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+    }
 
     @Test
     void itShouldCallEditQuizFromServiceAndReturnGetAllQuizzes(){
@@ -67,8 +69,46 @@ class QuizControllerTest {
         assertEquals(expectedQuiz, result);
     }
     @Test
-    void itShouldCallDeleteQuizFromServiceAndReturnGetAllQuizzes(){
-
+    void itShouldCallDeleteQuestionFromServiceAndReturnGetAllQuizzes() {
+        Long questionId = 0L;
+        ArrayList<Quiz> expectedQuiz = new ArrayList<>();
+        TestRestTemplate rest = new TestRestTemplate();
+        final String url = "http://localhost:" + port + "/deleteQuestion/" + questionId;
+        HttpEntity<Quiz> request = new HttpEntity<>(new Quiz());
+        doThrow(new ResponseStatusException(HttpStatus.ACCEPTED))
+                .when(service).deleteQuestion(questionId);
+        when(service.getAllQuizzes()).thenReturn(expectedQuiz);
+        Iterable<Quiz> result = service.getAllQuizzes();
+        ResponseEntity<Void> response = rest.exchange(url, HttpMethod.DELETE, request, Void.class);
+        assertEquals(HttpStatus.ACCEPTED, response.getStatusCode());
+        assertEquals(expectedQuiz, result);
     }
 
+    @Test
+    void itShouldCallDeleteQuizFromServiceAndReturnGetAllQuizzes(){
+        Long quizTempId = 0L;
+        ArrayList<Quiz> expectedQuiz = new ArrayList<>();
+        TestRestTemplate rest = new TestRestTemplate();
+        final String url = "http://localhost:" + port + "/deleteQuiz/" + quizTempId;
+        HttpEntity<Quiz> request = new HttpEntity<>(new Quiz());
+        doThrow(new ResponseStatusException(HttpStatus.ACCEPTED))
+                .when(service).deleteQuiz(quizTempId);
+        when(service.getAllQuizzes()).thenReturn(expectedQuiz);
+        Iterable<Quiz> result = service.getAllQuizzes();
+        ResponseEntity<Void> response = rest.exchange(url, HttpMethod.DELETE, request, Void.class);
+        assertEquals(HttpStatus.ACCEPTED, response.getStatusCode());
+        assertEquals(expectedQuiz, result);
+    }
+
+    @Test
+    void itShouldCallAndReturnGetAllQuizzesFromService(){
+        ArrayList<Quiz> expectedQuiz = new ArrayList<>();
+        TestRestTemplate rest = new TestRestTemplate();
+        final String url = "http://localhost:" + port + "/getAllQuizzes";
+        when(service.getAllQuizzes()).thenReturn(expectedQuiz);
+        ResponseEntity<Iterable> response = rest.getForEntity(url, Iterable.class);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(expectedQuiz, response.getBody());
+    }
 }
